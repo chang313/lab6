4405270891b151dc91d345f75d58b2b5ee654a57
Changhyeon Park <pch313@kaist.ac.kr>
2018-11-22 17:00:40 +0900

    first commit

--- mm.c
+++ mm.c
@@ -18,6 +18,32 @@
 #include "mm.h"
 #include "memlib.h"
 
+
+/* Basic constants and macros */
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1<<12) 
+
+#define MAX(x,y) ((x) > (y)? (x) : (y))
+
+#define PACK(size, alloc) ((size | (alloc))
+
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = val)
+
+#define GET_SISE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+#define NEXT_FBLP(bp) GET(bp)
+#define PREV_FBLP(bp) GET((char *)(bp) + WSIZE)
+
+
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
@@ -27,11 +53,57 @@
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+
+/* helper functions */
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+
+/* hepler function implementation */
+
+static void *extend_heap(size_t words) 
+{ 
+    char *bp;
+    size_t size;
+
+    /* ALlocate an even number of words to maintain alignmnent */
+    size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+    if ((long) (bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    /* Initialize free block header/footer and the epilogue header */
+    PUT(HDRP(bp), PACK(size, 0)); /* Free block header */
+    PUT(FTRP(bp), PACK(size, 0)); /* Free block footer */
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0,1)); /* New epilogue header */
+
+    /* Coalesce if the previous block was free */
+    return coalesce(bp);
+}
+
+static void *coalesce(void *bp) 
+{
+   
+}
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
-{
+{   
+    char *heap_listp;
+
+    /* Create the initial empty heap */
+    if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *)-1)
+        return -1;
+    PUT(heap_listp, 0);	/* Alignment padding */
+    PUT(heap_listp + (1*WSIZE), PACK(DSIZE,1)); /* Prologue header */
+    PUT(heap_listp + (2*WSIZE), PACK(DSIZE,1)); /* Prologue footer */
+    PUT(heap_listp + (3*WSIZE), PACK(DSIZE,1)); /* Epilogue header */
+    head_listp += 2*WSIZE;
+
+    /* Extend the empty heap with a free block of CHUNKSIZE byte */
+    if (extend_heap(CHUNKSIZE/WSIZE) == NULL)
+        return -1;
+
     return 0;
 }
 
